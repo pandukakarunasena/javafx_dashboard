a50e0f484dcbb6785e1c3f364ad8dd852a3e312c
panduka <kandyman777@gmail.com>
2021-02-20 09:39:30 +0530

    added customer form and in memory database

---
+++ src/database/Database.java
@@ -0,0 +1,9 @@
+package database;
+
+import java.util.ArrayList;
+
+public class Database {
+    ArrayList<Customer> customerList = new ArrayList<>();
+
+
+}
---
+++ src/sample/CustomerFormController.java
@@ -0,0 +1,14 @@
+package sample;
+
+import javafx.event.ActionEvent;
+
+public class CustomerFormController {
+    public void onClickActionAddCustomer(ActionEvent actionEvent) {
+    }
+
+    public void onClickActionEditCustomer(ActionEvent actionEvent) {
+    }
+
+    public void onClickActionDeleteCustomer(ActionEvent actionEvent) {
+    }
+}
