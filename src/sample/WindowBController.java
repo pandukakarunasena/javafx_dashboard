fa5337024d95b91e660893e42c87d1b2dff180fd
panduka <kandyman777@gmail.com>
2021-02-20 09:36:26 +0530

    added customer form and functionalities

--- src/sample/WindowBController.java
+++ src/sample/WindowBController.java
@@ -5,6 +5,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 import javafx.stage.Window;
 
@@ -13,10 +14,17 @@ import java.io.IOException;
 public class WindowBController {
     public Button btnBack;
     public AnchorPane windowB;
+    public Button btnCreateCustomerForm;
+    public Pane customerFormPane;
 
     public void onClickAction(ActionEvent actionEvent) throws IOException {
         Stage window = (Stage) windowB.getScene().getWindow();
         window.setScene(new Scene(FXMLLoader.load(getClass().getResource("sample.fxml"))));
         window.centerOnScreen();
+    }
+
+    public void onClickCreateCustomerForm(ActionEvent actionEvent) throws IOException{
+        System.out.println("button clicked");
+
     }
 }
